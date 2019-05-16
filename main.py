d09d6fea4c94d59aeaf79e9de6fcdb66ebe43566
Josh Bright <josh.bright@gmail.com>
2019-05-15 17:21:34 -0700

    Removed old versions of the game from the code

--- main.py
+++ main.py
@@ -7,205 +7,6 @@ from Engine.Config import get_screenrect, set_screensize
 from Engine.Text import text_surface
 
 
-class TetrisBlockOld:
-    def __init__(self, name, color):
-        self.name = name
-        self.x = 144
-        self.y = 16
-        self.image = None
-        self.width = None
-        self.height = None
-        self.blocks = []
-        self.block_size = 16
-        self.rotation = 0
-        self.color = color
-        self.setup(name)
-
-    def setup(self, name):
-        if name == "t":
-            self.blocks.append([0, 1, 0])
-            self.blocks.append([1, 1, 1])
-        elif name == "straight":
-            self.blocks.append([1])
-            self.blocks.append([1])
-            self.blocks.append([1])
-            self.blocks.append([1])
-        elif name == "normal_l":
-            self.blocks.append([1, 0])
-            self.blocks.append([1, 0])
-            self.blocks.append([1, 1])
-        elif name == "backwards_l":
-            self.blocks.append([0, 1])
-            self.blocks.append([0, 1])
-            self.blocks.append([1, 1])
-        elif name == "normal_zigzag":
-            self.blocks.append([1, 0])
-            self.blocks.append([1, 1])
-            self.blocks.append([0, 1])
-        elif name == "backwards_zigzag":
-            self.blocks.append([0, 1])
-            self.blocks.append([1, 1])
-            self.blocks.append([1, 0])
-
-    def rotate(self, direction):
-        self.width, self.height = self.height, self.width
-        if direction == "right":
-            self.blocks = list(zip(*self.blocks[::-1]))
-        elif direction == "left":
-            self.blocks = list(zip(*self.blocks[::]))
-        self.render_image()
-
-    def get_blocks(self):
-        result = []
-        for y in range(len(self.blocks)):
-            for x in range(len(self.blocks[0])):
-                if self.blocks[y][x] == 1:
-                    new_block = Tetris
-                    result.append(pygame.Rect(x * self.block_size, y * self.block_size, self.block_size, self.block_size))
-        return result
-
-    def draw(self, canvas):
-        canvas.blit(self.image, (self.x, self.y))
-
-    def get_rect(self):
-        return pygame.Rect(self.x, self.y, self.width, self.height)
-
-    def copy(self):
-        t = TetrisBlock(self.name, self.color)
-        t.x = self.x
-        t.y = self.y
-        return t
-
-    def __str__(self):
-        return "{}, {} - {} ({})".format(self.x, self.y, self.name, self.color)
-
-
-class TetrisOld:
-    def __init__(self):
-        self.screenrect = get_screenrect()
-        self.block_names = ["t", "straight", "normal_l", "backwards_l"]
-        self.play_rect = pygame.Rect(16, 16, 320, 560)
-        self.next_block_image = pygame.Surface((100, 100), pygame.SRCALPHA)
-        self.image = pygame.Surface((self.play_rect.width, self.play_rect.height), pygame.SRCALPHA)
-        self.image.fill((50, 50, 50))
-        self.block_colors = itertools.cycle([
-            (255, 0, 200),
-            (255, 0, 0),
-            (0, 255, 0),
-            (0, 0, 255),
-            (0, 255, 200),
-            (255, 200, 0),
-        ])
-        self.blocks = []
-        self.move_speed = 400
-        self.move_ticker = 0
-        t = TetrisBlock(random.choice(self.block_names), next(self.block_colors))
-        t2 = TetrisBlock(random.choice(self.block_names), next(self.block_colors))
-        self.blocks.append(t)
-        self.current_block = t
-        self.next_block = t2
-        self.render_next_block_window()
-
-    def draw(self, canvas):
-        canvas.blit(self.image, (self.play_rect.x, self.play_rect.y))
-        canvas.blit(self.next_block_image, (self.play_rect.right + 16, 16))
-        for block in self.blocks:
-            block.draw(canvas)
-
-    def render_next_block_window(self):
-        self.next_block_image = pygame.Surface((100, 100))
-        x = (100 - self.next_block_image.get_rect().width) // 2
-        y = (100 - self.next_block_image.get_rect().height) // 2
-        self.next_block_image.blit(self.next_block.image, (x, y))
-
-    def update(self, dt):
-        self.move_ticker += dt
-        if self.move_ticker > self.move_speed:
-            self.move_ticker = 0
-            self.update_blocks()
-            
-    def update_blocks(self):
-        new_rect = self.current_block.get_rect()
-        new_rect.y += self.current_block.block_size
-        new_block = self.current_block.copy()
-        new_block.y = new_rect.y
-        if self.check_block(new_block):
-            print("Updating current block position")
-            self.current_block.x = new_rect.x
-            self.current_block.y = new_rect.y
-        else:
-            self.spawn_block()
-
-    def spawn_block(self):
-        block_type = random.choice(self.block_names)
-        block_color = next(self.block_colors)
-        block = TetrisBlock(block_type, block_color)
-
-        self.current_block = self.next_block.copy()
-        self.blocks.append(self.current_block)
-        self.next_block = block
-        self.render_next_block_window()
-
-        return block
-
-    def check_block(self, block):
-        if not self.play_rect.contains(block.get_rect()):
-            print("check_block returning because rect is not within play_rect")
-            return False
-
-        for a_block in self.blocks:
-            print("Checking block", a_block)
-            if a_block == self.current_block:
-                print("block is same block skipping")
-                continue
-            if a_block.get_rect().colliderect(block.get_rect()):
-                print("piece collided with rect, checking blocks")
-                block_list = a_block.get_blocks()
-                target_block_list = block.get_blocks()
-                for b in block_list:
-                    collide_list = b.collidelistall(target_block_list)
-                    if collide_list:
-                        for c in collide_list:
-                            print(c)
-        return True
-
-    def handle_event(self, event):
-        if self.current_block is None:
-            return
-
-        event_type = None
-        if event.type == pygame.KEYUP:
-            new_rect = self.current_block.get_rect()
-            if event.key == pygame.K_RIGHT:
-                new_rect.x += self.current_block.block_size
-                event_type = "move"
-            elif event.key == pygame.K_LEFT:
-                new_rect.x -= self.current_block.block_size
-                event_type = "move"
-            elif event.key == pygame.K_DOWN:
-                new_rect.y += self.current_block.block_size
-                event_type = "move"
-            elif event.key == pygame.K_q:
-                self.current_block.rotate("left")
-                event_type = "rotate"
-            elif event.key == pygame.K_e:
-                self.current_block.rotate("right")
-                event_type = "rotate"
-            elif event.key == pygame.K_SPACE:
-                print(self.current_block.get_blocks())
-            if event_type == "move":
-                new_block = self.current_block.copy()
-                new_block.x = new_rect.x
-                new_block.y = new_rect.y
-                can_move = self.check_block(new_block)
-                if can_move:
-                    self.current_block.x = new_rect.x
-                    self.current_block.y = new_rect.y
-                else:
-                    print("Can't move there")
-
-
-
 class Tetrimino:
     def __init__(self, name, color, size):
         self.x = size * 2 + 16
@@ -218,7 +19,7 @@ class Tetrimino:
         self.image = None
         self.blocks = []
         self.setup(self.name)
-    
+
     def setup(self, name):
         if name == "t":
             self.blocks.append([0, 1, 0])
@@ -253,7 +54,7 @@ class Tetrimino:
         self.render_image()
 
     def rotate(self, direction):
-        # we need to be able to roll this back if 
+        # we need to be able to roll this back if
         # the shape gets off of the play area
         # after a rotate
         # may have to just build a new piece and test it
@@ -271,14 +72,26 @@ class Tetrimino:
             for x in range(len(self.blocks[0])):
                 if self.blocks[y][x] == 1:
                     if parent_offset:
-                        result.append((pygame.Rect(
-                            (self.x) + (x * self.size),
-                            (self.y - self.size) + (y * self.size),
-                            self.size, 
-                            self.size
-                        ), self.color))
+                        result.append(
+                            (
+                                pygame.Rect(
+                                    (self.x) + (x * self.size),
+                                    (self.y - self.size) + (y * self.size),
+                                    self.size,
+                                    self.size,
+                                ),
+                                self.color,
+                            )
+                        )
                     else:
-                        result.append((pygame.Rect(x * self.size, y * self.size, self.size, self.size), self.color))
+                        result.append(
+                            (
+                                pygame.Rect(
+                                    x * self.size, y * self.size, self.size, self.size
+                                ),
+                                self.color,
+                            )
+                        )
         return result
 
     def get_rect(self):
@@ -302,31 +115,52 @@ class Tetrimino:
 class Tetris:
     def __init__(self):
         self.screenrect = get_screenrect()
-        self.block_names = ["t", "straight", "normal_l", "backwards_l", "normal_zigzag", "backwards_zigzag", "square"]
-        self.tiles_wide = 8
-        self.tiles_high = 10
+        self.block_names = [
+            "t",
+            "straight",
+            "normal_l",
+            "backwards_l",
+            "normal_zigzag",
+            "backwards_zigzag",
+            "square",
+        ]
+        self.tiles_wide = 10
+        self.tiles_high = 20
         self.grid_size = 24
         self.padding = 16
-        self.play_rect = pygame.Rect(self.padding, self.padding, self.tiles_wide * self.grid_size, self.tiles_high * self.grid_size)
-        
+        self.play_rect = pygame.Rect(
+            self.padding,
+            self.padding,
+            self.tiles_wide * self.grid_size,
+            self.tiles_high * self.grid_size,
+        )
+
         self.grid = Grid(self.generate_blank_grid())
-        self.image = pygame.Surface((self.play_rect.width, self.play_rect.height), pygame.SRCALPHA)
+        self.image = pygame.Surface(
+            (self.play_rect.width, self.play_rect.height), pygame.SRCALPHA
+        )
         self.background_image = None
         self.blocks = []
         self.tick_count = 0
         self.piece_count = 0
         self.move_counter = 0
         self.move_timer = 300
-        self.block_colors = itertools.cycle([
-            (255, 0, 200),
-            (255, 0, 0),
-            (0, 255, 0),
-            (0, 0, 255),
-            (0, 255, 200),
-            (255, 200, 0),
-        ])
-        self.current_block = Tetrimino(random.choice(self.block_names), next(self.block_colors), self.grid_size)
-        self.next_block = Tetrimino(random.choice(self.block_names), next(self.block_colors), self.grid_size)
+        self.block_colors = itertools.cycle(
+            [
+                (255, 0, 200),
+                (255, 0, 0),
+                (0, 255, 0),
+                (0, 0, 255),
+                (0, 255, 200),
+                (255, 200, 0),
+            ]
+        )
+        self.current_block = Tetrimino(
+            random.choice(self.block_names), next(self.block_colors), self.grid_size
+        )
+        self.next_block = Tetrimino(
+            random.choice(self.block_names), next(self.block_colors), self.grid_size
+        )
         self.render_grid()
         self.render_ui()
 
@@ -335,8 +169,16 @@ class Tetris:
         self.render_next_block()
         tick_text = text_surface("Ticks: {}".format(self.tick_count), font_size=36)
         piece_text = text_surface("Pieces: {}".format(self.piece_count), font_size=36)
-        self.background_image.blit(tick_text, (self.play_rect.right + self.padding, 400))
-        self.background_image.blit(piece_text, (self.play_rect.right + self.padding, 400 + tick_text.get_rect().height + self.padding))
+        self.background_image.blit(
+            tick_text, (self.play_rect.right + self.padding, 400)
+        )
+        self.background_image.blit(
+            piece_text,
+            (
+                self.play_rect.right + self.padding,
+                400 + tick_text.get_rect().height + self.padding,
+            ),
+        )
 
     def pixel_to_tile(self, pos):
         if type(pos) == int:
@@ -348,11 +190,15 @@ class Tetris:
             return (x, y)
 
     def render_background(self):
-        self.background_image = pygame.Surface((self.screenrect.width, self.screenrect.height), pygame.SRCALPHA)
+        self.background_image = pygame.Surface(
+            (self.screenrect.width, self.screenrect.height), pygame.SRCALPHA
+        )
         self.background_image.fill((0, 0, 0))
 
     def render_next_block(self):
-        self.background_image.blit(self.next_block.image, (self.play_rect.right + 16, 16))
+        self.background_image.blit(
+            self.next_block.image, (self.play_rect.right + 16, 16)
+        )
 
     def generate_blank_grid(self):
         result = []
@@ -365,9 +211,6 @@ class Tetris:
     def tick(self):
         self.tick_count += 1
         if self.current_block:
-            # new_block = Tetrimino(self.current_block.name, self.current_block.color, self.current_block.size)
-            # new_block.y = new_rect.y
-            cur_rect = self.current_block.get_rect()
             self.current_block.y += self.current_block.size
             can_move = self.check_block(self.current_block)
             if not can_move:
@@ -382,7 +225,9 @@ class Tetris:
             if not self.grid.set_cell(gx, gy, block[1]):
                 print("Unable to set cell: ", gx, gy, block[1])
         self.current_block = self.next_block
-        self.next_block = Tetrimino(random.choice(self.block_names), next(self.block_colors), self.grid_size)
+        self.next_block = Tetrimino(
+            random.choice(self.block_names), next(self.block_colors), self.grid_size
+        )
         self.render_next_block()
         self.update_grid()
         self.check_for_rows()
@@ -402,19 +247,26 @@ class Tetris:
                 print(new_row)
                 self.grid.grid.insert(0, new_row)
             self.update_grid()
-        
+
     def print_grid(self):
         for row in self.grid.grid:
             print(row)
 
     def update_grid(self):
-        self.image = pygame.Surface((self.play_rect.width, self.play_rect.height), pygame.SRCALPHA)
+        self.image = pygame.Surface(
+            (self.play_rect.width, self.play_rect.height), pygame.SRCALPHA
+        )
         self.render_grid()
         for y in range(self.grid.height):
             for x in range(self.grid.width):
                 cell = self.grid.get_cell(x, y)
                 if cell:
-                    cell_rect = pygame.Rect(x * self.grid_size, y * self.grid_size, self.grid_size, self.grid_size)
+                    cell_rect = pygame.Rect(
+                        x * self.grid_size,
+                        y * self.grid_size,
+                        self.grid_size,
+                        self.grid_size,
+                    )
                     pygame.draw.rect(self.image, cell, cell_rect.inflate(-4, -4))
 
     def check_block(self, block):
@@ -438,7 +290,12 @@ class Tetris:
     def render_grid(self):
         for y in range(self.grid.height):
             for x in range(self.grid.width):
-                r = pygame.Rect(x * self.grid_size, y * self.grid_size, self.grid_size, self.grid_size)
+                r = pygame.Rect(
+                    x * self.grid_size,
+                    y * self.grid_size,
+                    self.grid_size,
+                    self.grid_size,
+                )
                 pygame.draw.rect(self.image, (100, 100, 100), r.inflate(-1, -1), 1)
                 if y == 0:
                     if x == self.grid.width - 1:
@@ -471,11 +328,20 @@ class Tetris:
         if event.type == pygame.KEYUP:
             test_block = None
             if event.key == pygame.K_RIGHT:
-                test_block = self.current_block.copy(x=self.current_block.x + self.current_block.size, y=self.current_block.y)
+                test_block = self.current_block.copy(
+                    x=self.current_block.x + self.current_block.size,
+                    y=self.current_block.y,
+                )
             elif event.key == pygame.K_LEFT:
-                test_block = self.current_block.copy(x=self.current_block.x - self.current_block.size, y=self.current_block.y)
+                test_block = self.current_block.copy(
+                    x=self.current_block.x - self.current_block.size,
+                    y=self.current_block.y,
+                )
             elif event.key == pygame.K_DOWN:
-                test_block = self.current_block.copy(x=self.current_block.x, y=self.current_block.y + self.current_block.size)
+                test_block = self.current_block.copy(
+                    x=self.current_block.x,
+                    y=self.current_block.y + self.current_block.size,
+                )
 
             if test_block and self.check_block(test_block):
                 self.current_block.x = test_block.x
